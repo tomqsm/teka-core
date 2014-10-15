63bf332c0033a5aa48b9a787e087e473d8a81875
Tomasz Kuśmierczyk <tomqsm@yahoo.co.uk>
2014-10-15 02:23:32 +0200

    changed annotations in testing class i.e. to annotations

--- image-base64/src/test/java/biz/letsweb/image/base64/ImageUtilsTest.java
+++ image-base64/src/test/java/biz/letsweb/image/base64/ImageUtilsTest.java
@@ -5,48 +5,55 @@ import java.io.File;
 import java.io.IOException;
 import javax.imageio.ImageIO;
 import junit.framework.TestCase;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
 
 /**
- *
+ * 
  * @author toks
  */
 public class ImageUtilsTest extends TestCase {
-    
-    public ImageUtilsTest(String testName) {
-        super(testName);
-    }
-    
-    @Override
-    protected void setUp() throws Exception {
-        super.setUp();
-    }
-    
-    @Override
-    protected void tearDown() throws Exception {
-        super.tearDown();
-    }
 
-    /**
-     * Test of decodeToImage method, of class ImageUtils.
-     * <img src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="First slide">
-     */
-    public void testDecodeToImageTets() throws IOException {
-        BufferedImage img = ImageIO.read(new File("src/main/resources/ajax-loader.gif"));
-        String imgstr = ImageUtils.encodeToString(img, "gif");
-        System.out.println("data:image/gif;base64," + imgstr);
-    }
+  @BeforeClass
+  public static void setUpClass() throws Exception {}
 
-    /**
-     * Test of encodeToString method, of class ImageUtils.
-     */
-    public void testEncodeToString() throws IOException {
-        BufferedImage img = ImageIO.read(new File("src/main/resources/bcg.gif"));
-        BufferedImage newImg;
-        String imgstr;
-        imgstr = ImageUtils.encodeToString(img, "gif");
-        System.out.println(imgstr);
-        newImg = ImageUtils.decodeToImage(imgstr);
-        ImageIO.write(newImg, "gif", new File("src/main/resources/output/bcg.gif"));
-    }
-    
+  @AfterClass
+  public static void tearDownClass() throws Exception {}
+
+  @Before
+  public void setUp() throws Exception {}
+
+  @After
+  public void tearDown() throws Exception {}
+
+
+  /**
+   * Test of decodeToImage method, of class ImageUtils. <img
+   * src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw=="
+   * alt="First slide">
+   */
+  @Test
+  public void testDecodeToImageTets() throws IOException {
+    BufferedImage img = ImageIO.read(new File("src/main/resources/ajax-loader.gif"));
+    String imgstr = ImageUtils.encodeToString(img, "gif");
+    System.out.println("data:image/gif;base64," + imgstr);
+  }
+
+  /**
+   * Test of encodeToString method, of class ImageUtils.
+   */
+  @Test
+  public void testEncodeToString() throws IOException {
+    BufferedImage img = ImageIO.read(new File("src/main/resources/bcg.gif"));
+    BufferedImage newImg;
+    String imgstr;
+    imgstr = ImageUtils.encodeToString(img, "gif");
+    System.out.println(imgstr);
+    newImg = ImageUtils.decodeToImage(imgstr);
+    ImageIO.write(newImg, "gif", new File("src/main/resources/output/bcg.gif"));
+  }
+
 }
